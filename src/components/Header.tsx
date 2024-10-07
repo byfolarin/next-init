1081da7693de5e5bbeca5e666a66a17ffbedbe04
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-08 00:47:50 +0100

    byte

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image"
+
+
 export default function Header() {
   return (
     <div>
       <header>
-     
+        <Image src='https://bytegrad.com/course-assets/youtube/example-logo.png'
+        alt="Logo description"
+        width={50}
+        height={50}
+        />
       </header>
     </div>
   )
