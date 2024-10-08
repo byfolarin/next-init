3545061b38123e036cead74683cb345f1b34efd7
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-08 14:07:54 +0100

    dynamic-routes

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,11 +1,19 @@
+import Link from "next/link";
 
+export default async function Page() {
+  const response = await fetch('https://dummyjson.com/posts?limit=10');
+  const data = await response.json();
 
-export default function Page() {
   return (
-    <main className="text-center pt-32 px-5">
-       <h1 className="text-4xl font-bold mb-5 md:text-5xl">
-        All Posts
-      </h1>
+    <main className="text-center pt-16 px-5">
+      <h1 className="text-4xl font-bold mb-5 md:text-5xl">All Posts</h1>
+      <ul>
+        {data.posts.map((post) => (
+          <li key={post.id} className="mb-2">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
-  )
+  );
 }
