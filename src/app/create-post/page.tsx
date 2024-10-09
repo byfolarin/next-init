96c71b41c8914e461832e49ea1e7cfddfe699a66
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-09 12:23:39 +0100

    api

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -1,6 +1,15 @@
 import { createPost } from "@/actions/actions";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function ProtectedRoute() {
+  const { isAuthenticated } = getKindeServerSession();
+  
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login?post_login_redirect_url=/create-post");
+  }
+
+
   return (
     <main className="text-center pt-16">
              <h1 className="text-4xl font-bold mb-5 md:text-5xl">Create Post</h1>
