3537712133b911a42f5f51a7d2b47d4f9325043a
Folarin Folarin <thefolafolarin@gmail.com>
2024-10-13 00:09:54 +0100

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 export default function middleware(req: NextRequest) {
   return withAuth(req);
 }
-
+ 
 
 export const config ={
     matcher:["/create-post"]
